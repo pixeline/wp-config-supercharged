bc383ab5794b4b9dfed271510123cb3464ca00b7
Alexandre Plennevaux <alexandre@pixeline.be>
2016-02-20 21:47:42 +0100

    Major updates
    Loads of new tricks.
    Much better overall organisation and hierarchy.

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-wp-config-supercharged
-======================
+# wp-config.php Super Charged.
 
-All useful wordpress globals in a wp-config.php, ready for a new project. 
+All wordpress configuration constants in a wp-config.php, ready for a new project. 
 Set your variables and just remove what you don't need.
 
 It is pre-configured to play with a local DEV server and a PRODUCTION ("live") server.
+
+## Need a STAGING server configuration?
+
+Copy the 'live' case, as a Staging is supposed to mirror the production server.
+
+# Credits
+
+Most of these tricks were learned by reading
+
+- http://codex.wordpress.org/Editing_wp-config.php
+- http://premium.wpmudev.org/blog/wordpress-wp-config-file-guide/
+
+# Discuss / Share tricks ?
+- use this repo's Issues, submit pull requests. You know the drill.
--- wp-config.php
+++ wp-config.php
@@ -1,141 +1,150 @@
 <?php
-
-/*
+/*****************************************************************************
 	SUPERCHARGED WP-CONFIG.PHP
 
-	All useful wordpress globals in a wp-config.php, ready for a new project. Set your variables and just remove what you don't need.
-
-	author: Alexandre Plennevaux _ alexandre@pixeline.be _ http://pixeline.be
-
-	read the doc: http://codex.wordpress.org/Editing_wp-config.php
+	@Description:
+		All useful wordpress globals in a wp-config.php, ready for a new project.
 
 
-*/
+	@Usage:
+		Adapt the settings to your need: there is a switch allowing you to use different configurations (e.g: "local", "staging", "production"). Set your variables and just remove what you don't need.
 
+	@Learn:
+		Basic: 		http://codex.wordpress.org/Editing_wp-config.php
+		Advanced:	http://premium.wpmudev.org/blog/wordpress-wp-config-file-guide/
 
-/*
-	MEMORY ALLOCATED TO WORDPRESS _ useful if your site shows many 500 errors and when allowing heavy file uploads. default : 32M
-*/
-define('WP_MEMORY_LIMIT', '256M');
+	@Discuss: 	https://github.com/pixeline/wp-config-supercharged/issues
 
-// Increase memory in the admin area specifically
-define('WP_MAX_MEMORY_LIMIT', '256M');
+	@Author:
+		Alexandre Plennevaux _ alexandre@pixeline.be _ https://pixeline.be
 
+*****************************************************************************/
 
-/*
-	SWITCH BETWEEN YOUR DEV AND YOUR PRODUCTION SERVER _ damn useful.
-*/
-define('CURRENT_SERVER','live');
+// The Main Switch:
+define('CURRENT_SERVER','local');
+
+/*  ------------------------ YOUR SERVER CONFIGURATIONS --------------------- */
 
 switch(CURRENT_SERVER){
 
-case 'dev':
+case 'local':
 
-	// DEVELOPMENT SERVER. OPTIMIZE FOR DEBUGGING
+	/***************************************
+	DEVELOPMENT SERVER. OPTIMIZED FOR DEBUGGING
+	****************************************/
 
 	define('WP_CACHE', false);
-	define('WP_DEBUG', true);
-	define('WP_POST_REVISIONS', false );
-	define('AUTOSAVE_INTERVAL', 150); // in seconds
-	define('EMPTY_TRASH_DAYS', 7); // in days (use 0 to disable trash)
-	/*
-		The SAVEQUERIES definition saves the database queries to an array and that array can be displayed to help analyze those queries. The information saves each query, what function called it, and how long that query took to execute. See http://codex.wordpress.org/Editing_wp-config.php on how to use it.
-	*/
+	define('WP_DEBUG', 'local');
 	define('SAVEQUERIES', false);
+	define('WP_DEBUG_LOG', false);
+	define('WP_DEBUG_DISPLAY', true);
+	/*
+		The SAVEQUERIES definition saves the database queries to an array to help analyze those queries.
+		See http://codex.wordpress.org/Editing_wp-config.php on how to use it.
+	*/
+	define( 'SCRIPT_DEBUG', true );
+	define( 'CONCATENATE_SCRIPTS', false );
 
-	$customer['dbname']= 'DATABASE_NAME';
-	$customer['dbuser']='DATABASE_USER';
-	$customer['pass']='DB_PASSWORD';
-	$customer['dbhost']='localhost';
+	// DATABASE
+	define('DB_NAME', 'DATABASE_NAME');
+	define('DB_USER', 'DATABASE_USER');
+	define('DB_PASSWORD', 'DB_PASSWORD');
+	define('DB_HOST', 'localhost');
 
-	$customer['db_prefix']  = 'cstmr_';  // default wp_ again. Don't make it too easy for hackers to know you're using wordpress.
-	$customer['cookie_domain'] = 'customer.dev';
-	$customer['wp_home'] = 'http://'.$customer['cookie_domain'];
-	$customer['wp_siteurl'] = 'http://'.$customer['cookie_domain'];
+	// DOMAIN & URL
+	define('PROTOCOL', 'http://');
+	define('DOMAIN_NAME', 'domain.dev');
+	define('WP_SITEURL', PROTOCOL . DOMAIN_NAME);
+	define('PATH_TO_WP', '/'); // if your WordPress is in a subdirectory.
+	define('WP_HOME', WP_SITEURL . PATH_TO_WP); // root of your WordPress install
 
 	break;
 
 case 'live':
-
-	// PRODUCTION SERVER. OPTIMIZE FOR SPEED.
-	define('WP_CACHE', true);
+default:
+	/***************************************
+	PRODUCTION SERVER. OPTIMIZED FOR SPEED.
+	****************************************/
+	define('WP_CACHE', false);
 	define('WP_DEBUG', false);
+	define('SAVEQUERIES', false);
+	define( 'SCRIPT_DEBUG', false);
+	define( 'CONCATENATE_SCRIPTS', true );
+	// log errors in a file (wp-content/debug.log), don't show them to end-users.
+	define('WP_DEBUG_LOG', true);
+	define('WP_DEBUG_DISPLAY', false);
 
-	// authors will be able to go back up to X earlier versions of their posts if they need to.
-	define('WP_POST_REVISIONS', 2);
-	define('AUTOSAVE_INTERVAL', 150); // in seconds
-	define('EMPTY_TRASH_DAYS', 7); // in days (use 0 to disable trash)
+	// DATABASE
+	define('DB_NAME', 'DATABASE_NAME');
+	define('DB_USER', 'DATABASE_USER');
+	define('DB_PASSWORD', 'DB_PASSWORD');
+	define('DB_HOST', 'localhost');
 
-	$customer['dbname']= 'DATABASE_NAME';
-	$customer['dbuser']='DATABASE_USER';
-	$customer['pass']='DB_PASSWORD';
-	$customer['dbhost'] ='localhost';
-	$customer['db_prefix']  = 'cstmr_';
+	// DOMAIN & URL
+	define('PROTOCOL', 'http://');
+	define('DOMAIN_NAME', 'domain.tld');
+	define('WP_SITEURL', PROTOCOL . DOMAIN_NAME);
+	define('PATH_TO_WP', '/'); // if your WordPress is in a subdirectory.
+	define('WP_HOME', WP_SITEURL . PATH_TO_WP); // root of your WordPress install
+	// Using subdomains to serve static content (CDN) ? 
+	// To prevent WordPress cookies from being sent with each request to static content on your subdomain, set the cookie domain to your non-static domain only.
+	// define('COOKIE_DOMAIN', DOMAIN_NAME);
 
-	$customer['cookie_domain'] = 'customer.com';
-	$customer['wp_home'] = 'http://'.$customer['cookie_domain'];
-	$customer['wp_siteurl'] = 'http://'.$customer['cookie_domain'];
-
+	// FTP: On some webhosting configurations, Wordpress automatic updates fail. Try the FTP method. If still a no-go, see: http://codex.wordpress.org/Editing_wp-config.php#Override_of_default_file_permissions for alternative methods. */
 	/*
-		On some webhosting configurations, Wordpress automatic updates fail. Try the FTP method should work.
-		If still a no-go, see: http://codex.wordpress.org/Editing_wp-config.php#Override_of_default_file_permissions for alternative methods.
-	*/
-
 	define('FS_METHOD', 'ftpext');
 	define('FTP_USER', 'YOUR FTP LOGIN');
 	define('FTP_PASS', 'YOUR FTP PASSWORD');
 	define('FTP_HOST', 'YOUR FTP HOST (without http:// or ftp://)');
 	define('FTP_SSL', false);
+*/
 	break;
 }
 
-/*
-	WORDPRESS DATABASE CONNECTION PARAMETERS _
-*/
-define('DB_NAME', $customer['dbname']);
-define('DB_USER', $customer['dbuser']);
-define('DB_PASSWORD', $customer['pass']);
-define('DB_HOST', $customer['dbhost']);
 
-/*
-	WORDPRESS HOME URLS _ (no need to make these changeable via WP-ADMIN.)
-*/
-define('WP_HOME', $customer['wp_home']);
-define('WP_SITEURL', $customer['wp_siteurl']);
+/*  ------------------------ SETTINGS COMMON TO ALL SERVERS  --------------------- */
 
-/*
-		The domain set in the cookies for WordPress can be specified for those with unusual domain setups. One reason is if subdomains are used to serve static content. To prevent WordPress cookies from being sent with each request to static content on your subdomain you can set the cookie domain to your non-static domain only.
-	*/
-define('COOKIE_DOMAIN', $customer['cookie_domain']);
+define('TABLE_PREFIX','cstmr_');  // Something else than the default wp_. Only numbers, letters, and underscores.
+define('WP_POST_REVISIONS', 5 ); // How many revisions to keep at max.
+define('AUTOSAVE_INTERVAL', 120); // in seconds
+define('EMPTY_TRASH_DAYS', 7); // in days (use 0 to disable trash)
 
-/*
-	WORDPRESS CHARACTER SET _
-*/
+// WORDPRESS' LANGUAGE _ Default is 'en_EN'
+define('WPLANG', 'fr_FR');
+
+// DB INTERNALS
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
+// DIRECTORY CUSTOMIZATION
+// make it less obvious that your site is using wordpress.
 
-/*
-	WORDPRESS INTERNAL FOLDER STRUCTURE _ useful to make it less obvious to prying eyes that your site is using wordpress.
-*/
+// rename wp-content folder
+// define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+// define( 'WP_CONTENT_URL', WP_SITEURL.'/wp-content');
 
-/*
-	Moving wp-content folder
-*/
-define( 'WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content' );
-define( 'WP_CONTENT_URL', WP_SITEURL.'/wp-content');
-/*
-	Moving plugins folder
-*/
-define( 'WP_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins' );
-define( 'WP_PLUGIN_URL', WP_SITEURL.'/wp-content/plugins');
-// For compatibility with old plugins.
-define( 'PLUGINDIR',  WP_PLUGIN_DIR );
+// rename uploads folder
+// define( 'UPLOADS', '/wp-content/uploads' );
 
-/*
-	Moving uploads folder
-*/
-define( 'UPLOADS', '/blog/wp-content/uploads' );
+// rename plugins folder
+// define( 'WP_PLUGIN_DIR', dirname(__FILE__)  . '/wp-content/plugins' );
+// define( 'WP_PLUGIN_URL', WP_SITEURL.'/wp-content/plugins');
+
+// You cannot move the Themes folder, but your can register an additional theme directory
+// register_theme_directory( dirname( __FILE__ ) . '/themes-dev' );
+
+// Prevent users from editing themes and plugins via the UI
+define( 'DISALLOW_FILE_MODS', true );
+define( 'DISALLOW_FILE_EDIT', true );
+
+// Cron system
+//define( 'DISABLE_WP_CRON', true ); // If you can, disable wp_cron: use a real cronjob to trigger /wp-cron.php
+
+// SSL
+if (PROTOCOL === 'https://'){
+	define( 'FORCE_SSL_LOGIN', true );
+	define( 'FORCE_SSL_ADMIN', true );
+}
 
 
 /**#@+
@@ -155,56 +164,69 @@ define('AUTH_SALT',        'put something here');
 define('SECURE_AUTH_SALT', 'put something here');
 define('LOGGED_IN_SALT',   'put something here');
 define('NONCE_SALT',       'put something here');
-/**#@-*/
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = $customer['db_prefix'];
-
-/**
- WORDPRESS USERS' LANGUAGE _ Default is 'en_EN'
- */
-
-define('WPLANG', 'fr_FR');
-
-// Useful to localize the global php environment, not just wordpress functions.
-setlocale(LC_ALL, 'fr_FR');
 
 
-// Faster administration area by disabling Javascript Concatenation
-define( 'CONCATENATE_SCRIPTS', false );
-
+/*  ------------------------  MULTISITE MODE  --------------------- */
 
 /*
-	ERROR LOGGING _
- */
+define( 'WP_ALLOW_MULTISITE', true ); // Enable multisite mode. 
+define('SUBDOMAIN_INSTALL', false); //  Leave false to use subdirectories
+define('DOMAIN_CURRENT_SITE', DOMAIN_NAME);
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
-if (WP_DEBUG) {
-	error_reporting(E_ALL | E_WARNING |  E_ERROR);
-	// display errors
-	@ini_set('log_errors','Off');
-	@ini_set('display_errors','On');
-	define('WP_DEBUG_LOG', false);
-	define('WP_DEBUG_DISPLAY', true);
-}else{
-	error_reporting(E_WARNING | E_ERROR);
-	// log errors in a file (wp-content/debug.log), don't show them to end-users.
-	@ini_set('log_errors','On');
-	@ini_set('display_errors','Off');
-	define('WP_DEBUG_LOG', true);
-	define('WP_DEBUG_DISPLAY', false);
-}
+// Must-use Plugins. These plugins are loaded by default before any other plugins.
+// define( 'WPMU_PLUGIN_DIR', dirname(__FILE__) . '/extensions/builtin' );
+// define( 'WPMU_PLUGIN_URL', 'http://mywebsite.com/extensions/builtin' );
+
+// Where to redirect users stumbling on a 404 website
+define( 'NOBLOGREDIRECT', 'http://mainwebsite.com' );
+
+
+*/
+
+
+/*  ------------------------ OTHER CONSTANTS YOU COULD NEED  --------------------- */
+
+/*
+
+// MEMORY ALLOCATION
+define('WP_MEMORY_LIMIT', '32M');
+define('WP_MAX_MEMORY_LIMIT', '32M');  // Admin area specifically
+define('WP_DEFAULT_THEME', 'twentyeleven'); // Custom Default Theme
+
+// Custom Database Table for Users
+define( 'CUSTOM_USER_TABLE', $table_prefix.'peeps' );
+define( 'CUSTOM_USER_META_TABLE', $table_prefix.'peepmeta' );
+
+// More Cron
+define( 'WP_CRON_LOCK_TIMEOUT', 120 ); // cron repeat interval
+define( 'ALTERNATE_WP_CRON', true ); // Issues with cron? Try this setting as a last resort.
+
+// Auto-updates
+define( 'AUTOMATIC_UPDATER_DISABLED', true );  // Disable all automatic updates
+define( 'WP_AUTO_UPDATE_CORE', false ); // Disable all core updates
+define( 'WP_AUTO_UPDATE_CORE', true ); // Enable all core updates, including minor and major
+define( 'WP_AUTO_UPDATE_CORE', 'minor' ); // Enable core updates for minor releases (default)
+define( 'DO_NOT_UPGRADE_GLOBAL_TABLES', true ); // Disable DB Tables auto-update
+
+*/
 
 /*  -------------------------- STOP EDITING PAST THIS POINT  --------------------- */
+if(WP_DEBUG_LOG){
+	@ini_set('log_errors','On');
+}
+if(WP_DEBUG_DISPLAY){
+	@ini_set('display_errors','On');
+}
+// Adapt your servers to the chosen locale.
+setlocale(LC_ALL, WPLANG);
 
+// For compatibility with old plugins
+define( 'PLUGINDIR',  WP_PLUGIN_DIR );
 
-/** Chemin absolu vers le dossier de WordPress. */
+/** Absolute path to WordPress. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . PATH_TO_WP);
 require_once(ABSPATH . 'wp-settings.php');
